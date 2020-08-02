62c520241cff6fc0b055717dd2ca6ff446646d0d
Joaom123 <joaomarcus12769@gmail.com>
2020-08-02 15:15:37 -0300

    resolvido

---
+++ listas-encadeadas/exercicios/4-7-6-contagem.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../listaEncadeadaComCabeca.h"
+
+int contagem(celula *lista1) {
+    int numeroDeCelulas = 0;
+    
+    while (lista1->proximaCelula != NULL)
+    {
+        lista1 = lista1->proximaCelula;
+        numeroDeCelulas += 1;
+    }
+    
+    return numeroDeCelulas;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    celula *cabecaDaLista1 = criaCelula(0, criaLista());
+
+    printf("%d", contagem(cabecaDaLista1));
+    
+    return EXIT_SUCCESS;
+}
