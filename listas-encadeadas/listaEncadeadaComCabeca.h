006a1d9bccaeca9e34ab6f080c1ef5dd5f1b248c
Joaom123 <joaomarcus12769@gmail.com>
2020-08-01 23:31:00 -0300

    transformando lista encadeada em header

--- listas-encadeadas/listaEncadeadaComCabeca.h
+++ listas-encadeadas/listaEncadeadaComCabeca.h
@@ -101,11 +101,3 @@ celula *criaLista() {
 void imprimeSeparador() {
     printf("---------------------\n");
 }
-
-int main(int argc, char const *argv[]) {
-    celula *cabecaDaLista = criaCelula(0, criaLista()); //Cabeça da lista, seu conteúdo não importa
-    imprimeLista(cabecaDaLista->proximaCelula);
-    imprimeSeparador();
-
-    return EXIT_SUCCESS;
-}
