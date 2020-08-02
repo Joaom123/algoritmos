12f3ef13b80cbbfda3a0761ea70e41ba4ffe7645
Joaom123 <joaomarcus12769@gmail.com>
2020-08-01 23:31:08 -0300

    primeiro exercicio

---
+++ listas-encadeadas/exercicios/4-7-1-vetorParaLista.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../listaEncadeadaComCabeca.h"
+
+//Escreva uma função que copie um vetor para uma lista encadeada.
+
+celula *copiaArrayParaLista(int inteiros[]) {
+    celula *celCabeca = criaCelula(0, NULL);
+    celula *celAux = celCabeca; //Guarda referência do primeiro valor
+
+    for (int i = 0; i < 5; i++)
+    {
+        celCabeca->proximaCelula = criaCelula(inteiros[i], NULL);
+        celCabeca = celCabeca->proximaCelula;
+    }
+
+    return celAux->proximaCelula;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int inteiros [5] = {0, 1, 2, 3, 4};
+    celula *cabecaDaLista = criaCelula(0, copiaArrayParaLista(inteiros));
+    
+    imprimeLista(
+        cabecaDaLista->proximaCelula
+    );
+
+    return 0;
+}
