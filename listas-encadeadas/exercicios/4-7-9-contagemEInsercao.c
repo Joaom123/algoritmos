dc84358e811d9343b5db9de0925362abb59cc1f0
Joaom123 <joaomarcus12769@gmail.com>
2020-08-02 16:01:27 -0300

    4-7-9 resolvida

---
+++ listas-encadeadas/exercicios/4-7-9-contagemEInsercao.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../listaEncadeadaComCabeca.h"
+
+void contagemEInsercao(int numeroDaCelula, int conteudo, celula *lista1) {
+    int numeroDeCelulas = 0;
+    
+    while (lista1->proximaCelula != NULL)
+    {
+        numeroDeCelulas += 1;
+        lista1 = lista1->proximaCelula;
+        
+        if (numeroDeCelulas == numeroDaCelula)
+        {
+            celula *cel = criaCelula(conteudo, lista1->proximaCelula);
+            lista1->proximaCelula = cel; 
+            return;
+        }
+        
+    }
+
+    return;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    celula *cabecaDaLista1 = criaCelula(0, criaLista());
+
+    contagemEInsercao(1, 10, cabecaDaLista1);
+
+    imprimeLista(cabecaDaLista1->proximaCelula);
+
+    return EXIT_SUCCESS;
+}
