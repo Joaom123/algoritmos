97d410a5dac59a618083b4b0b24a4c64c3ee342e
João Marcus <joaomarcus12769@gmail.com>
2020-10-27 16:47:31 -0300

    alterando fila_vetor.c para fila_vetor.h

---
+++ filas/fila_vetor.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define N 100
+
+static int fila[N];
+static int indexInicio, indexFim;
+
+void cria (void) {
+    indexInicio = indexFim = 0;
+}
+
+void insere (int valor) {
+   fila[indexFim++] = valor;
+}
+
+int removeC (void) {
+   return fila[indexInicio++];
+}
+
+int filaEstaVazia (void) {
+   return indexInicio >= indexFim;
+}
+
+int filaEstaCheia (void) {
+   return indexFim == N;
+}
