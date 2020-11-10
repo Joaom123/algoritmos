545577dc6d5f5a32f14f9b8574d42d7126fb3bc0
João Marcus <joaomarcus12769@gmail.com>
2020-11-09 21:09:43 -0300

    chamadas dos suites de testes das pilhas

---
+++ pilhas/pilhas.h
@@ -0,0 +1,13 @@
+//
+// Created by joaomarcus on 09/11/2020.
+//
+
+#ifndef ALGORITMOS_PILHAS_H
+#define ALGORITMOS_PILHAS_H
+#include "pilha_vetor_test.h"
+
+void pilhas() {
+    pilha_vetor_test_suite();
+}
+
+#endif //ALGORITMOS_PILHAS_H
