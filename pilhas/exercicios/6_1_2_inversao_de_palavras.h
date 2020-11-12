e098be3aca49287952b2772541aa5dff81ea500c
João Marcus <joaomarcus12769@gmail.com>
2020-11-12 01:26:36 -0300

    Começando exercicio 6-1-2 inversao de palavras

---
+++ pilhas/exercicios/6_1_2_inversao_de_palavras.h
@@ -0,0 +1,58 @@
+//
+// Created by joaomarcus on 12/11/2020.
+//
+#include <stdio.h>
+#include <z3.h>
+
+#ifndef ALGORITMOS_6_1_2_INVERSAO_DE_PALAVRAS_H
+#define ALGORITMOS_6_1_2_INVERSAO_DE_PALAVRAS_H
+
+char pegaLetra(const char *frase, int indexDaFrase);
+int letraEUmEspaco(char letra);
+bool naoEFimDaFrase(const char *frase, int indexDaFrase);
+
+
+//6.1.2 Inversão de palavras.
+// Escreva uma função que inverta a ordem das letras de cada palavra de uma sentença,
+// preservando a ordem das palavras. Suponha que as palavras da sentença são separadas por espaços.
+// A aplicação da operação à sentença
+// AMU MEGASNEM ATERCES, por exemplo, deve produzir UMA MENSAGEM SECRETA.
+
+
+int inversaoDePalavras(char *frase) {
+    //Todo Separar as palavras por espaço
+    int indexDaFrase = 0;
+
+    // Uma frase é composta por várias palavras
+    char *palavra;
+    int indexDaPalavra = 0;
+
+
+    while (naoEFimDaFrase(frase, indexDaFrase)) {
+        char letra = pegaLetra(frase, indexDaFrase);
+        ++indexDaFrase;
+        palavra[indexDaPalavra] = letra;
+
+        //Guardar palavra
+        if(letraEUmEspaco(letra)) {
+
+            indexDaPalavra = 0;
+        }
+    }
+    //Todo Adicionar tais palavras a uma pilha
+    //Todo Desempilha e Imprime tudo
+}
+
+bool naoEFimDaFrase(const char *frase, int indexDaFrase) {
+    return pegaLetra(frase, indexDaFrase) != '\0';
+}
+
+char pegaLetra(const char *frase, int indexDaFrase) {
+    return frase[indexDaFrase];
+}
+
+int letraEUmEspaco(char letra) {
+    return letra == 32;
+}
+
+#endif //ALGORITMOS_6_1_2_INVERSAO_DE_PALAVRAS_H
