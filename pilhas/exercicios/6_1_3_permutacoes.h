a006de9f0f1b58657aad1051d5ff1e6405886d8d
João Marcus <joaomarcus12769@gmail.com>
2020-11-12 20:08:37 -0300

    adicionando exercicio 6-1-3

---
+++ pilhas/exercicios/6_1_3_permutacoes.h
@@ -0,0 +1,14 @@
+//
+// Created by joaomarcus on 12/11/2020.
+//
+
+#ifndef ALGORITMOS_6_1_3__H
+#define ALGORITMOS_6_1_3__H
+// 6.1.3 Permutações produzidas pelo desempilhar [10, sec. 2.2.1].
+// Suponha que os números inteiros 1, 2, 3, 4 são colocados, nesta ordem, numa pilha inicialmente vazia.
+// Depois de cada operação de empilhar, você pode retirar zero ou mais elementos da pilha.
+// Cada número retirado da pilha é impresso numa folha de papel.
+// Por exemplo, a sequência de operações E, E, D, E, D, D, E, D, onde E significa “empilhar o próximo
+// número da sequência” e D significa “desempilhar”, produz a impressão da sequência 2, 3, 1, 4.
+// Quais das 24 permutações de 1, 2, 3, 4 podem ser obtidas desta maneira?
+#endif //ALGORITMOS_6_1_3__H
