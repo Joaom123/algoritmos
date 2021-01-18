362ad06835913ef8f4b4b08c6d40a8822798d6af
João Marcus <joaomarcus12769@gmail.com>
2021-01-18 04:06:02 -0300

    começando mergesort

---
+++ mergesort/mergesort.h
@@ -0,0 +1,8 @@
+//
+// Created by joaomarcus on 18/01/2021.
+//
+
+#ifndef ALGORITMOS_MERGESORT_H
+#define ALGORITMOS_MERGESORT_H
+
+#endif //ALGORITMOS_MERGESORT_H
--- mergesort/mergesortTest.c
+++ mergesort/mergesortTest.c
@@ -2,12 +2,6 @@
 // Created by joaomarcus on 18/01/2021.
 //
 
-#include <stdio.h>
-#include "mergesort.h"
-
 int main() {
 
-    return 0;
 }
-
-
