9959ece867d47d815637f4172a5cc15c2bc2e6c2
João Marcus <joaomarcus12769@gmail.com>
2021-01-19 00:09:33 -0300

    alterando o indexFim para coincidir com n-1 ao inves de n

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,10 +1,11 @@
 //
 // Created by joaomarcus on 18/01/2021.
 //
+
 #include <stdio.h>
 #include "mergesort.h"
 
-int tamanhoDoVetor = 10;
+int tamanhoDoVetor = 15;
 
 void imprimeVetor(int vetor[]) {
     for (int i = 0; i < tamanhoDoVetor; i++)
@@ -13,12 +14,12 @@ void imprimeVetor(int vetor[]) {
 }
 
 int main() {
-    int vetor[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int vetor[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5};
 
     printf("Vetor não-ordenado: ");
     imprimeVetor(vetor);
 
-    mergesort(vetor, 0, tamanhoDoVetor );
+    mergesort(vetor, 0, tamanhoDoVetor - 1);
 
     printf("Vetor ordenado: ");
     imprimeVetor(vetor);
--- mergesort/mergesort.h
+++ mergesort/mergesort.h
@@ -5,21 +5,22 @@
 #ifndef ALGORITMOS_MERGESORT_H
 #define ALGORITMOS_MERGESORT_H
 #include <stdlib.h>
+#include <math.h>
 
 //Combina dois vetores ordenados, vetor[indexInicio, ..., indexMeio - 1] e vetor[indexMeio, ..., indexFim - 1]
 //em um vetor ordenado, vetor[indexInicio, ..., indexFim - 1]
 static void merge(int vetor[], int indexInicio, int indexMeio, int indexFim) {
     int *vetorAuxiliar;
-    int tamanhoDoVetor = indexFim - indexInicio;
+    int tamanhoDoVetor = indexFim - indexInicio + 1;
     vetorAuxiliar = malloc(tamanhoDoVetor * sizeof(int));
 
     int i = indexInicio;
-    int j = indexMeio;
+    int j = indexMeio + 1;
     int k = 0;
 
     //Preenche o vetor auxiliar comparando os dois vetores, quando um deles for totalmente utiizado
     //a iteração para.
-    while (i < indexMeio && j < indexFim) {
+    while (i <= indexMeio && j <= indexFim) {
         if (vetor[i] <= vetor[j])
             vetorAuxiliar[k++] = vetor[i++];
         else
@@ -27,13 +28,13 @@ static void merge(int vetor[], int indexInicio, int indexMeio, int indexFim) {
     }
 
     //Como o resto do vetor que sobrou não foi inserido no vetorAuxiliar, precisamos inserir o que sobrou
-    while (i < indexMeio)
+    while (i <= indexMeio)
         vetorAuxiliar[k++] = vetor[i++];
-    while (j < indexFim)
+    while (j <= indexFim)
         vetorAuxiliar[k++] = vetor[j++];
 
     //Copia-se o vetor auxiliar para o vetor original
-    for (i = indexInicio; i < indexFim; i++)
+    for (i = indexInicio; i <= indexFim; i++)
         vetor[i] = vetorAuxiliar[i - indexInicio];
 
     //libera a memória alocada pelo vetorAuxiliar
@@ -43,15 +44,16 @@ static void merge(int vetor[], int indexInicio, int indexMeio, int indexFim) {
 void mergesort(int vetor[], int indexInicio, int indexFim) {
     //caso de saida da recorrência,
     //ocorre quando há apenas um elemento no array
-    if (indexInicio >= indexFim - 1)
+    if (indexInicio >= indexFim)
         return;
 
     int indexMeio;
-    indexMeio = (indexInicio + indexFim)/2;
+    indexMeio = floor((indexInicio + indexFim)/2);
+//    printf("%d\n", indexMeio);
 
     mergesort(vetor, indexInicio, indexMeio);
-    mergesort(vetor, indexMeio, indexFim);
+    mergesort(vetor, indexMeio + 1, indexFim);
     merge(vetor, indexInicio, indexMeio, indexFim);
 }
-    
+
 #endif //ALGORITMOS_MERGESORT_H
