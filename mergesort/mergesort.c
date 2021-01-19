1970eee3fd5135482d94149d43238637bc2757ba
João Marcus <joaomarcus12769@gmail.com>
2021-01-18 22:31:41 -0300

    implementação do mergesort

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,11 +1,26 @@
 //
 // Created by joaomarcus on 18/01/2021.
 //
-
 #include <stdio.h>
 #include "mergesort.h"
 
+void imprimeVetor(int *vetor) {
+    int i;
+    for (i = 0; i < 6; i++)
+        printf("%d ", vetor[i]);
+    printf("\n");
+}
+
 int main() {
+    int vetor[] = {4, 2, 1, 2, 4, 6};
+
+    printf("Vetor não-ordenado: ");
+    imprimeVetor(vetor);
+
+    mergesort(vetor, 0, 5);
+
+    printf("Vetor ordenado: ");
+    imprimeVetor(vetor);
 
     return 0;
 }
--- mergesort/mergesort.h
+++ mergesort/mergesort.h
@@ -4,5 +4,44 @@
 
 #ifndef ALGORITMOS_MERGESORT_H
 #define ALGORITMOS_MERGESORT_H
+#include <math.h>
+#include <stdlib.h>
 
+static void merge(int vetor[], int indexInicio, int indexMeio, int indexFim) {
+    int *vetorAuxiliar;
+    int tamanhoDoVetor = indexFim - indexInicio;
+    vetorAuxiliar = malloc(tamanhoDoVetor*sizeof(int));
+
+    int i = indexInicio;
+    int j = indexMeio;
+    int k = 0;
+
+    while (i < indexMeio && j < indexFim) {
+        if (vetor[i] <= vetor[j])
+            vetorAuxiliar[k++] = vetor[i++];
+        else
+            vetorAuxiliar[k++] = vetor[j++];
+    }
+
+    while (i < indexMeio)
+        vetorAuxiliar[k++] = vetor[i++];
+    while (j < indexFim)
+        vetorAuxiliar[k++] = vetor[j++];
+    for (i = indexInicio; i < indexFim; ++i)
+        vetor[i] = vetorAuxiliar[i-indexInicio];
+
+    free(vetorAuxiliar);
+}
+
+void mergesort(int vetor[], int indexInicio, int indexFim) {
+    if (indexInicio >= indexFim)
+        return;
+
+    int indexMeio;
+    indexMeio = (indexInicio + indexFim)/2;
+    mergesort(vetor, indexInicio, indexMeio);
+    mergesort(vetor, indexMeio + 1, indexFim);
+    merge(vetor, indexInicio, indexMeio, indexFim);
+}
+    
 #endif //ALGORITMOS_MERGESORT_H
